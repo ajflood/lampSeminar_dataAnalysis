aa3d5dc4530b088e0858f066bbdc5a0f1840404f
Aaron <aaronjflood@gmail.com>
2019-03-12 14:07:00 -0500

    Added commands to clean the data set a little

--- data_analysis.py
+++ data_analysis.py
@@ -6,9 +6,15 @@ filename = "clean_usgs_data.txt"
 
 #loading the data file into pandas
 database = pandas.read_csv(filename, sep='\t')
+#print(database)
 
-print(database)
+##renaming the columns to make more human readable
+database = database.rename(columns={"75905_00065_30800": "gauge_height", "75906_00060_00003": "discharge"})
+#print(database)
 
-
+#deleting some unnecessary rows and columns
+database = database.drop(['75905_00065_30800_cd', '75906_00060_00003_cd', 'agency_cd', 'site_no'], axis=1)
+database = database.drop([0], axis=0)
+#print(database)
 
 
