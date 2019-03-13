7b42974d1f064f1484caaaa81fff559335eba7ff
Aaron <aaronjflood@gmail.com>
2019-03-13 10:26:56 -0500

    Added stats examples

--- stats_example.py
+++ stats_example.py
@@ -1,7 +1,20 @@
 import pandas
+import numpy
 import statsmodels.api as statsmodels
 import statsmodels.formula.api as formulastats
 
-#loading a data set from statsmodels for examples purposes
+# loading a data set from statsmodels for examples purposes
 data = statsmodels.datasets.get_rdataset("Guerry", "HistData").data
 print(data)
+
+# Fit regression model (using the natural log of one of the regressors)
+results = formulastats.ols('Lottery ~ Literacy + numpy.log(Pop1831)', data=data).fit()
+print(results.summary())
+
+# Fit a different regression model with interactions
+results = formulastats.ols('Lottery ~ Lottery + Instruction + Lottery*Instruction', data=data).fit()
+print(results.summary())
+
+# Fit a different regression model with categorical variables
+results = formulastats.ols('Lottery ~ Lottery + Instruction + C(Region)', data=data).fit()
+print(results.summary())
